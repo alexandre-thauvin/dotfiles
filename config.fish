13707f1164f11df628fa3e3f2c22ef8f335421e0
Alexandre Thauvin <alexandre.thauvin@digitalstrom.com>
2022-08-18 10:28:59 +0200

    add some scripts and clean repo

---
+++ config.fish
@@ -0,0 +1,24 @@
+for f in ~/.config/fish/functions/*.fish
+  source $f
+ end
+
+set --export ANDROID_HOME /opt/android-studio;
+set -gx PATH $ANDROID_HOME/tools $PATH;
+set -gx PATH $ANDROID_HOME/tools/bin $PATH;
+set -gx PATH $ANDROID_HOME/platform-tools $PATH;
+set -gx PATH $ANDROID_HOME/emulator $PATH;
+set --export SNAP /snap 
+
+set --export JAVA_HOME /opt/android-studio/jre;
+set -gx PATH "/home/linuxbrew/.linuxbrew/bin:/opt/android-studio/bin:/bin:/sbin:/usr/bin:/usr/local/share/dotnet:/usr/local/bin:/usr/sbin:/usr/local/share:/flutter/bin:/home/alexandrethauvin/dotefiles/scripts:$JAVA_HOME" $PATH;
+set VIRTUAL_ENV "/home/alexandrethauvin/venv"
+
+alias grep='grep --color'
+alias shutdown='systemctl poweroff -i'
+alias ne='emacs'
+alias ls='ls -l --color=auto'
+alias grep='grep --color'
+alias refresh="source ~/.config/fish/config.fish"
+alias dlt='mr_clean'
+
+thefuck --alias | source
