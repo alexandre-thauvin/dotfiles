1792435df0746796426ab43e0eac7f5d91ecea54
Alexandre Thauvin <alexandre.thauvin@digitalstrom.com>
2022-11-26 15:05:38 +0100

    update fish config

--- config.fish
+++ config.fish
@@ -10,8 +10,8 @@ set -gx PATH $ANDROID_HOME/emulator $PATH;
 set --export SNAP /snap 
 
 set --export JAVA_HOME /opt/android-studio/jre;
-set -gx PATH "/var/lib/snapd/snap/bin:/opt/android-studio/bin:/bin:/sbin:/usr/bin:/usr/local/share/dotnet:/usr/local/bin:/usr/sbin:/usr/local/share:/flutter/bin:/home/toto/dotfiles/scripts:$JAVA_HOME" $PATH;
-set VIRTUAL_ENV "/home/toto/venv"
+set -gx PATH "/home/toto/.local/bin:/var/lib/snapd/snap/bin:/opt/android-studio/bin:/bin:/sbin:/usr/bin:/usr/local/share/dotnet:/usr/local/bin:/usr/sbin:/usr/local/share:/flutter/bin:/home/toto/dotfiles/scripts:$JAVA_HOME" $PATH;
+
 
 alias grep='grep --color'
 alias shutdown='systemctl poweroff -i'
