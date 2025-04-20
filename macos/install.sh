03a0df28eceb73d587e56fd1f34446e475936316
Alexandre Thauvin <athauvin@swissquote.ch>
2025-04-20 11:54:20 +0200

    Improve add luarocks and deps to install

--- macos/install.sh
+++ macos/install.sh
@@ -7,6 +7,12 @@ brew install sketchybar
 brew tap FelixKratz/formulae
 brew install sketchybar
 brew install font-hack-nerd-font
+brew install kitty
+brew install luarocks
+
+# Needed for sketchybar X aerospace
+luarocks install lua-cjson
+luarocks install luaposix
 
 # create and backup all config
 mkdir ~/.config/backup
@@ -17,6 +23,11 @@ mv ~/.config/kitty/kitty.conf ~/.config/backup/kitty/
 mv ~/.config/aerospace/aerospace.toml ~/.config/backup/aerospace/
 mv ~/.config/sketchybar ~/.config/backup/sketchybar
 mv `~/Library/Application Support/nushell/config.nu` ~/.config/backup/nushell/
+
+# create dirs
+mkdir ~/.config/aerospace
+mkdir ~/.config/starship
+mkdir ~/.config/kitty
 
 # symlink all config files
 ln -s ~/dotfiles/macos/config/nushell/config.nu `~/Library/Application Support/nushell/config.nu`
@@ -29,7 +40,7 @@ ln -s ~/dotfiles/common/config/starship/starship.toml ~/.config/starship/starshi
 brew services start sketchybar
 
 # make nushell as default shell
-sudo chsh -s /opt/homebrew/bin/nu
+sudo bash -c 'echo "/opt/homebrew/bin/nu" >> /etc/shells'
+chsh -s /opt/homebrew/bin/nu
 
-printf("\nAdd /opt/homebrew/bin/nu to /etc/shells")
-printf("\nYou can now launch aerospace")
+printf("You can now launch aerospace!")
